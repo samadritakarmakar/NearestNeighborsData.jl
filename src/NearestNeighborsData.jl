9fc2045de7bb5455d9872746c89c0f9c42739e29
Samadrita Karmakar <samadritakarmakar@gmail.com>
2022-07-28 10:52:24 +0200

    Added knnUniqueData to export

--- src/NearestNeighborsData.jl
+++ src/NearestNeighborsData.jl
@@ -7,5 +7,6 @@ include("nearestNeighboursData.jl")
 export RefData, addToRefData!
 #from nearestNeighboursData
 export AbstractDataTree, BallDataTree, KDDataTree, knnData, nnData, inrangeData
+export knnUniqueData
 
 end # module
